1077b991ba15c3b5c709732025aa47be486a2051
visakhbs123 <visakhbsundaresan@gmail.com>
2021-04-22 10:45:54 +0530

    todo app added

---
+++ todoapp/todo_project/todo_app/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import Todoforms
+
+
+# Create your views here.
+def task_view(request):
+    obj1 = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        priority = request.POST.get('priority')
+        date = request.POST.get('date')
+        obj = Task(name=name, priority=priority, date=date)
+        obj.save()
+    return render(request, 'task_view.html', {'obj1': obj1})
+
+
+def delete(request, taskid):
+    task = Task.objects.get(id=taskid)
+    if request.method == "POST":
+        task.delete()
+        return redirect('/')
+    return render(request, 'delete.html', {'task': task})
+
+
+def update(request, id):
+    task = Task.objects.get(id=id)
+    form = Todoforms(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'edit.html', {'task': task, 'form': form})
